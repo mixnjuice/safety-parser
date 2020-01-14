1718f2e7b75e3746936712f3699fc3b4dfbc5ea3
ayan4m1 <andrew@bulletlogic.com>
2020-01-14 01:02:54 -0500

    add database config, libs, and skeleton module

--- src/modules/config.js
+++ src/modules/config.js
@@ -2,6 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const database = {
+  host: process.env.SDS_DB_HOSTNAME,
+  user: process.env.SDS_DB_USERNAME,
+  password: process.env.SDS_DB_PASSWORD,
+  database: process.env.SDS_DB_DATABASE
+};
+
 export const logging = {
   level: process.env.SDS_LOG_LEVEL || 'info',
   timestampFormat: process.env.SDS_LOG_TIME_FMT
---
+++ src/modules/database.js
@@ -0,0 +1,82 @@
+import knex from 'knex';
+
+import { database as config } from 'modules/config';
+
+const client = knex({
+  client: 'pg',
+  connection: config
+});
+
+export const querySingle = async query => {
+  const results = await query;
+
+  if (!Array.isArray(results) || results.length === 0) {
+    return null;
+  }
+
+  return results[0];
+};
+
+export const queryMultiple = async query => {
+  const results = await query;
+
+  if (!Array.isArray(results)) {
+    return [];
+  }
+
+  return results;
+};
+
+export const findFlavor = (flavorName, vendorCode) =>
+  client('flavor as f')
+    .innerJoin('vendor as v', 'f.vendor_id', 'v.id')
+    .select('id', 'name', 'slug', 'density')
+    .select({
+      vendorId: 'v.id'
+    })
+    .where('f.name', 'like', `%${flavorName}%`)
+    .where('v.code', vendorCode);
+
+export const getIngredients = () =>
+  client('ingredient')
+    .select(['id', 'name', 'notes', 'created', 'updated'])
+    .select({
+      casNumber: 'cas_number',
+      ingredientCategoryId: 'ingredient_category_id'
+    });
+
+export const getIngredient = id => getIngredients().where('id', id);
+
+export const getCategories = () =>
+  client('ingredient_category').select([
+    'id',
+    'name',
+    'ordinal',
+    'description'
+  ]);
+
+export const getCategory = id => getCategories().where('id', id);
+
+export const getFlavorIngredients = () =>
+  client('flavors_ingredients').select({
+    flavorId: 'flavor_id',
+    ingredientId: 'ingredient_id'
+  });
+
+export const getFlavorIngredient = (flavorId, ingredientId) =>
+  getFlavorIngredients().where({
+    /* eslint-disable camelcase */
+    flavor_id: flavorId,
+    ingredient_id: ingredientId
+    /* eslint-enable camelcase */
+  });
+
+export const insertFlavorIngredient = (flavorId, ingredientId) =>
+  client('flavors_ingredients').insert({
+    /* eslint-disable camelcase */
+    flavor_id: flavorId,
+    ingredient_id: ingredientId,
+    /* eslint-enable camelcase */
+    created: client.fn.now(),
+    updated: client.fn.now()
+  });
