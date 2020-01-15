080a047c80238bf9d0d88a3d209268d7abf964c3
ayan4m1 <andrew@bulletlogic.com>
2020-01-14 19:03:51 -0500

    error handling and shutdown db in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import { closeDatabase } from 'modules/database';
+import { getLogger } from 'modules/logging';
 import { parseAllDirectories } from 'modules/parser';
 
+const log = getLogger('app');
+
 const execute = async () => {
-  await parseAllDirectories();
+  try {
+    await parseAllDirectories();
+  } catch (error) {
+    log.error(error.message);
+  } finally {
+    closeDatabase();
+  }
 };
 
 execute();
